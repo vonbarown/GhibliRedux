72727394d99bff70c564bb9f5562c71be5208c14
vonbarown <vonielbrown@pursuit.org>
2020-01-27 20:25:57 -0500

    creating state and reducers

---
+++ src/Store/actions/counterActions.jsx
@@ -0,0 +1,9 @@
+import { INCREMENT, DECREMENT } from "./actionTypes";
+
+export const incrementCount = () => {
+    return { type: INCREMENT };
+};
+
+export const decrementCount = () => {
+    return { type: DECREMENT };
+};
